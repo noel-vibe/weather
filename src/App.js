ab3919ece4dd83c55240faaa2f30069ad5b63d82
noel-vibe <138129056+noel-vibe@users.noreply.github.com>
2024-03-25 01:39:34 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import { useEffect,useState } from 'react';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import WeatherBox from './component/WeatherBox';
-import WeatherButton from './component/WeatherButton';
+import React, { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { Container } from "react-bootstrap";
+import WeatherButton from "./component/WeatherButton";
+import WeatherBox from "./component/WeatherBox";
 import ClipLoader from "react-spinners/ClipLoader";
 
 //1. 앱이 실행되자마자 현재 위치기반의 날씨가 보인다.
@@ -12,73 +13,86 @@ import ClipLoader from "react-spinners/ClipLoader";
 //5. 현재위치 버튼을 누르면 다시 현재위치 기반의 날씨가 나온다
 //6. 데이터를 들고오는 동안 로딩 스피너가 돈다
 
-function App() {
-  const [weather,setWeather]=useState(null)
-  const [city,setCity]=useState('')
-  const [loading,setLoading]=useState(false)
-  const cities=["paris","new york","tokyo","seoul"]
-  
-  const getCurrentLocation = () =>{
-    navigator.geolocation.getCurrentPosition((position)=>{
-      let lat = position.coords.latitude
-      let lon = position.coords.longitude
-      getWeatherByCurrentLocation(lat,lon)
-      console.log("현재위치",lat,lon)
+const cities = ["paris","new york","tokyo","seoul"];
+const API_KEY = "5932e0b7d653b20d4eb4cca9bc29d587";
+
+const App = () => {
+  const [weather, setWeather] = useState(null);
+  const [city, setCity] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [apiError, setAPIError] = useState("");
+
+  const getWeatherByCurrentLocation = async (lat, lon) => {
+    try {
+      let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+      const response = await fetch(url);
+      const data = await response.json();
+      setWeather(data);
+      setLoading(false);
+    } catch (err) {
+      setAPIError(err.message);
+      setLoading(false);
+    }
+  };
+
+  const getCurrentLocation = () => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const { latitude, longitude } = position.coords;
+      getWeatherByCurrentLocation(latitude, longitude);
     });
-  }
+  };
 
-  const getWeatherByCurrentLocation = async(lat,lon) =>{
-    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=5932e0b7d653b20d4eb4cca9bc29d587&units=metric`
-    setLoading(true)
-    let response = await fetch(url)
-    let data =await response.json()
-    setWeather(data)
-    setLoading(false)
-  }
+  const getWeatherByCity = async () => {
+    try {
+      let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+      const response = await fetch(url);
+      const data = await response.json();
+      setWeather(data);
+      setLoading(false);
+    } catch (err) {
+      console.log(err);
+      setAPIError(err.message);
+      setLoading(false);
+    }
+  };
 
-  const getWeatherByCity = async() => {
-    let url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=5932e0b7d653b20d4eb4cca9bc29d587&units=metric`
-    setLoading(true)
-    let response =await fetch(url)
-    let data = await response.json()
-    setWeather(data)
-    setLoading(false)
-  }
+  useEffect(() => {
+     if (city == null) {
+       setLoading(true);
+       getCurrentLocation();
+     } else {
+    setLoading(true);
+    getWeatherByCity();
+     }
+  }, [city]);
 
   const handleCityChange = (city) => {
-    if(city === "current") {
+    if (city === "current") {
       setCity(null);
     } else {
       setCity(city);
     }
   };
 
-  useEffect(()=>{
-    if(city===""){
-      getCurrentLocation()
-    }else{
-      getWeatherByCity()
-    }
-  },[city])
-
   return (
-    <div>{loading? (
-      <div className="container">
-            <ClipLoader
-              color="#f88c6b"
-              loading={loading}
-              size={150}
-              aria-label="Loading Spinner"
-              data-testid="loader"/>
-              </div>
-         ):(
-      <div className="container">
-        <WeatherBox weather ={weather}/>
-        <WeatherButton cities={cities} handleCityChange={handleCityChange} selectedCity={city}/>
+    <div>
+        {loading ? (
+          <div className="container">
+            <ClipLoader color="#gray" size={150} loading={loading} />
+          </div>
+        ) : !apiError ? (
+          <div class="container">
+            <WeatherBox weather={weather} />
+            <WeatherButton
+              cities={cities}
+              handleCityChange={handleCityChange}
+              selectedCity={city}
+            />
+          </div>
+        ) : (
+          apiError
+        )}
       </div>
-         )}
-    </div>
   );
-}
-
+};
 export default App;
--- src/component/WeatherButton.js
+++ src/component/WeatherButton.js
@@ -1,14 +1,23 @@
 import React from "react";
 import { Button } from "react-bootstrap";
 
-const WeatherButton = ({ cities, selectedCity,handleCityChange}) => {
-
+const WeatherButton = ({ cities, selectedCity, handleCityChange }) => {
   return (
     <div>
-      <Button variant={`${selectedCity == null ? "secondary" : "outline-secondary"}`} onClick={() => handleCityChange("current")}>Current Location</Button>
+      <Button
+        variant={`${selectedCity == null ? "secondary" : "outline-secondary"}`}
+        onClick={() => handleCityChange("current")} className="button"
+      >
+        현재 위치
+      </Button>
 
       {cities.map((city) => (
-        <Button variant={`${selectedCity == city ? "secondary" : "outline-secondary"}`} onClick={() => handleCityChange(city)}>
+        <Button
+          variant={`${
+            selectedCity == city ? "secondary" : "outline-secondary"
+          }`}
+          onClick={() => handleCityChange(city)} className="button"
+        >
           {city}
         </Button>
       ))}
